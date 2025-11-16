37093cc87cdad7d137cf994f8509b0d8d3f83da9
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-11-16 14:21:37 -0500

    Add script to install vulnerable OpenSSH version
    Script downloads and installs a specific vulnerable version of OpenSSH, preventing future updates.

---
+++ openssh/script.sh
@@ -0,0 +1,11 @@
+#!bin/sh
+
+wget http://snapshot.debian.org/archive/debian/20180801T000000Z/pool/main/o/openssh/openssh-server_7.4p1-10+deb9u3_amd64.deb
+wget http://snapshot.debian.org/archive/debian/20180801T000000Z/pool/main/o/openssh/openssh-sftp-server_7.4p1-10+deb9u3_amd64.deb
+
+# Install vulnerable version
+dpkg -i openssh-sftp-server_7.4p1-10+deb9u3_amd64.deb
+dpkg -i openssh-server_7.4p1-10+deb9u3_amd64.deb
+
+# Prevent updates
+apt-mark hold openssh-server openssh-sftp-server
