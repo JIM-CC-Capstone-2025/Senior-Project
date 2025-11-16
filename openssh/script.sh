58d84865fa4b89abe07e41a67db969c087854ed8
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-11-16 14:26:21 -0500

    Simplify installation of OpenSSH packages
    Combine installation commands for OpenSSH server and SFTP server.

--- openssh/script.sh
+++ openssh/script.sh
@@ -4,8 +4,9 @@ wget http://snapshot.debian.org/archive/debian/20180801T000000Z/pool/main/o/open
 wget http://snapshot.debian.org/archive/debian/20180801T000000Z/pool/main/o/openssh/openssh-sftp-server_7.4p1-10+deb9u3_amd64.deb
 
 # Install vulnerable version
-dpkg -i openssh-sftp-server_7.4p1-10+deb9u3_amd64.deb
-dpkg -i openssh-server_7.4p1-10+deb9u3_amd64.deb
+dpkg -i openssh-sftp-server_7.4p1-10+deb9u3_amd64.deb openssh-server_7.4p1-10+deb9u3_amd64.deb
 
 # Prevent updates
 apt-mark hold openssh-server openssh-sftp-server
+
+systemctl restart sshd
